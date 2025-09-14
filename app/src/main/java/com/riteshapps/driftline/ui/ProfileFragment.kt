bd667fd7a038ec313908e4638a00c597ed677b01
ritesh423 <riteshsingh.py@gmail.com>
2025-09-14 14:27:06 +0530

    Added Basic Profile Ui :lipstick:

--- app/src/main/java/com/riteshapps/driftline/ui/ProfileFragment.kt
+++ app/src/main/java/com/riteshapps/driftline/ui/ProfileFragment.kt
@@ -1,32 +1,21 @@
 package com.riteshapps.driftline.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.riteshapps.driftline.R
+import com.riteshapps.driftline.databinding.FragmentProfileBinding
 
-/**
- * A simple [androidx.fragment.app.Fragment] subclass.
- * Use the [WomenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class WomenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
-    
 }
